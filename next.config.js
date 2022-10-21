789e139b11f02c32104c8c2de735b3da4f22cfe0
scures <scurescu@suse.com>
2022-10-21 15:21:56 +0200

    next paths

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   // if hosted in Github pages, since the format is user.github.io/REPO_NAME and Next.js expects the assets to live under the main path /, we need to rewrite it with the repository name.
-  // assetPrefix: process.env.NEXT_PUBLIC_DEVELOPMENT ? '' : '/openzerotrust.io/',
-  // basePath: process.env.NEXT_PUBLIC_DEVELOPMENT ? '' : '/openzerotrust.io',
+  assetPrefix: process.env.NEXT_PUBLIC_DEVELOPMENT ? '' : '/openzerotrust.io/',
+  basePath: process.env.NEXT_PUBLIC_DEVELOPMENT ? '' : '/openzerotrust.io',
   images: {
     unoptimized: true,
   },
