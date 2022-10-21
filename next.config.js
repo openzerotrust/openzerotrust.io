04dbd9e341281c85d99fe44d6d658a91a0706e53
scures <scurescu@suse.com>
2022-10-21 15:19:31 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   // if hosted in Github pages, since the format is user.github.io/REPO_NAME and Next.js expects the assets to live under the main path /, we need to rewrite it with the repository name.
   // assetPrefix: process.env.NEXT_PUBLIC_DEVELOPMENT ? '' : '/openzerotrust.io/',
   // basePath: process.env.NEXT_PUBLIC_DEVELOPMENT ? '' : '/openzerotrust.io',
-  // images: {
-  //   unoptimized: true,
-  // },
+  images: {
+    unoptimized: true,
+  },
 }
