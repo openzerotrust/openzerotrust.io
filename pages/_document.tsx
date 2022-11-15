a1e4bb990f3866bb9a27681e730115e1343c407d
rmahique-github <>
2022-11-15 14:52:50 +0100

    add google tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,18 @@ export default function Document() {
       <body className='font-body'>
         <Main />
         <NextScript />
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EH6S6EJ2S3"></script>
+        <script dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-EH6S6EJ2S3');
+          `,
+          }}
+        />
+
       </body>
     </Html>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -191,15 +191,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-EH6S6EJ2S3"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-EH6S6EJ2S3');
-</script>
       </main>
     </>
   )
